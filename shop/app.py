cf4b82378097b235cc4fc88ebef69803655f181b
sleepSoud <elizarovlev27@gmail.com>
2025-03-30 15:44:37 +0300

    Update app.py

--- shop/app.py
+++ shop/app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask("магазин RR",
+            template_folder="",
+            static_folder="")
+
+@app.route('/')
+def  index():
+    return render_template("index.html")
+
+app.run(debug=True)
 
